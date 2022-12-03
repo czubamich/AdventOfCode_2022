07c0ccc3a883f4aaf23fe988a7173c825f41a4f5
Michael Czuba <michael.czuba@esky.com>
2022-12-03 11:21:03 +0100

    Add project files.

---
+++ AdventOfCode/DayTwo.cs
@@ -0,0 +1,103 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Disassemblers;
+
+
+[MemoryDiagnoser]
+public class DayTwo
+{
+    struct Match
+    {
+        const char Kam = 'A';
+        const char Pap = 'B';
+        const char Noz = 'C';
+
+        const char Prz = 'X';
+        const char Rem = 'Y';
+        const char Wyg = 'Z';
+
+        char[] XX;
+
+        public char A => XX[0];
+        public char B => XX[1];
+
+        public Match(char[] X) { XX = X; }
+
+        public static Match FromOne(char[] X) => new Match(new[] { X[0], Translate(X[1]) });
+
+        public static Match FromTwo(char[] X) => new Match(new[] { X[0], ResolveStrategy(X) });
+
+        public int BaseScore => B switch
+        {
+            Kam => 1,
+            Pap => 2,
+            Noz => 3,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        public int ResolveMatchScore()
+        {
+            if (A == B)
+                return 3;
+
+            if ((A == Pap && B == Noz)
+                || (A == Kam && B == Pap)
+                || (A == Noz && B == Kam))
+                return 6;
+
+            return 0;
+        }
+
+        private static char ResolveStrategy(char[] AM) => AM switch
+        {
+            [Pap, Prz] => Kam,
+            [Kam, Prz] => Noz,
+            [Noz, Prz] => Pap,
+            [Pap, Wyg] => Noz,
+            [Kam, Wyg] => Pap,
+            [Noz, Wyg] => Kam,
+            _ => AM[0]
+        };
+
+        private static char Translate(char X) => X switch
+        {
+            'X' => 'A',
+            'Y' => 'B',
+            'Z' => 'C',
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    public static int ProcessMatchOne()
+    {
+        static IEnumerable<Match> GetMatches(string fileName)
+            => File.ReadAllText(fileName)
+                .Split("\r\n")
+                .Select(f => Match.FromOne(f.Remove(1, 1).ToArray()));
+
+        return GetMatches("DayTwo.txt")
+            .Sum(m => m.BaseScore + m.ResolveMatchScore());
+    }
+
+    public static int ProcessMatchTwo()
+    {
+        static IEnumerable<Match> GetMatches(string fileName)
+            => File.ReadAllText(fileName)
+                .Split("\r\n")
+                .Select(f => Match.FromTwo(f.Remove(1, 1).ToArray()));
+
+        return GetMatches("DayTwo.txt")
+            .Sum(m => m.BaseScore + m.ResolveMatchScore());
+    }
+
+    [Benchmark]
+    public void BenchmarkOne()
+    {
+        var result = ProcessMatchOne();
+    }
+
+    [Benchmark]
+    public void BenchmarkTwo()
+    {
+        var result = ProcessMatchTwo();
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,7 @@
+﻿using BenchmarkDotNet.Running;
+
+Console.WriteLine(DayTwo.ProcessMatchTwo());
+
+Console.ReadKey();
+
+var summary = BenchmarkRunner.Run<DayTwo>();
