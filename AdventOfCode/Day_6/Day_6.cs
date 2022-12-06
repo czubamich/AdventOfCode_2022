8e610f42fd997bb6c341db208ecbf54487b61e0f
Michael Czuba <michael.czuba@esky.com>
2022-12-06 17:17:04 +0100

    Add Day 6 solution

---
+++ AdventOfCode/Day_6/Day_6.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode.Infrastructure;
+using System.Diagnostics;
+
+public class Day_6 : BaseDay, IDay
+{
+    public object PerformPartOne()
+    {
+        return SignalProcessor.FindFirstMarker(InputAsText, 4);
+    }
+
+    public object PerformPartTwo()
+    {
+        return SignalProcessor.FindFirstMarker(InputAsText, 14);
+    }
+}
+
+public class SignalProcessor
+{
+    public static int FindFirstMarker(string data, int expectedDistinct)
+    {
+        for (int i = expectedDistinct; i < data.Length; i++)
+            if (data[(i - expectedDistinct)..i].Distinct().Count() == expectedDistinct)
+                return i;
+        throw new UnreachableException();
+    }
+}
--- AdventOfCode/Infrastructure/BaseDay.cs
+++ AdventOfCode/Infrastructure/BaseDay.cs
@@ -1,9 +1,11 @@
-﻿namespace AdventOfCode.Infrastructure;
+﻿using FastSerialization;
+
+namespace AdventOfCode.Infrastructure;
 
 public abstract class BaseDay
 {
     protected string InputPath => $"Data/{GetType().Name}.txt";
-
     protected IEnumerable<string> InputAsLines => File.ReadLines(InputPath);
     protected string InputAsText => File.ReadAllText(InputPath);
+    protected Stream InputAsStream => File.OpenRead(InputPath);
 }
