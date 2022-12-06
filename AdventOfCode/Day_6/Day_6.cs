8b7553e7e2fc2cafdee0151eb8a81856fd409261
Michael Czuba <michael.czuba@esky.com>
2022-12-06 18:40:33 +0100

    Optimize Day 6 solution, Add input caching for benchmarks

--- AdventOfCode/Day_6/Day_6.cs
+++ AdventOfCode/Day_6/Day_6.cs
@@ -1,26 +1,22 @@
 ﻿using AdventOfCode.Infrastructure;
 using System.Diagnostics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 public class Day_6 : BaseDay, IDay
 {
     public object PerformPartOne()
     {
-        return SignalProcessor.FindFirstMarker(InputAsText, 4);
+        var dataAsArray = InputAsText.ToCharArray();
+        int i = 4;
+        while (dataAsArray[(i - 4)..i++].ToHashSet().Count != 4) ;
+        return --i;
     }
 
     public object PerformPartTwo()
     {
-        return SignalProcessor.FindFirstMarker(InputAsText, 14);
-    }
-}
-
-public class SignalProcessor
-{
-    public static int FindFirstMarker(string data, int expectedDistinct)
-    {
-        for (int i = expectedDistinct; i < data.Length; i++)
-            if (data[(i - expectedDistinct)..i].Distinct().Count() == expectedDistinct)
-                return i;
-        throw new UnreachableException();
+        var dataAsArray = InputAsText.ToCharArray();
+        int i = 14;
+        while (dataAsArray[(i - 14)..i++].ToHashSet().Count != 14) ;
+        return --i;
     }
 }
--- AdventOfCode/Infrastructure/BaseDay.cs
+++ AdventOfCode/Infrastructure/BaseDay.cs
@@ -4,8 +4,34 @@ namespace AdventOfCode.Infrastructure;
 
 public abstract class BaseDay
 {
+    private static IEnumerable<string>? _inputAsLines = null;
+    private static string? _inputAsText = null;
+    private static Type? _last = null;
+
     protected string InputPath => $"Data/{GetType().Name}.txt";
-    protected IEnumerable<string> InputAsLines => File.ReadLines(InputPath);
-    protected string InputAsText => File.ReadAllText(InputPath);
-    protected Stream InputAsStream => File.OpenRead(InputPath);
+    protected IEnumerable<string> InputAsLines
+    {
+        get
+        {
+            if(GetType() != _last)
+            {
+                _last = GetType(); _inputAsText = null; _inputAsLines = null;
+            }
+
+            return _inputAsLines ??= File.ReadLines(InputPath).ToArray();
+        }
+    }
+    
+    protected string InputAsText 
+    {
+        get
+        {
+            if (GetType() != _last)
+            {
+                _last = GetType(); _inputAsText = null; _inputAsLines = null;
+            }
+
+            return _inputAsText ??= File.ReadAllText(InputPath);
+        } 
+    }
 }
