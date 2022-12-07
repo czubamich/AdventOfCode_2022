17babd4f4bfc53bb172330367b524d1ce5b58f5c
Michael Czuba <czuba.mich@gmail.com>
2022-12-07 19:32:09 +0100

    Improve benchmark summary view

--- AdventOfCode/Infrastructure/DayRunner.cs
+++ AdventOfCode/Infrastructure/DayRunner.cs
@@ -20,8 +20,8 @@ public static class DayRunner
         return BenchmarkRunner.Run(benchmarkType,
             ManualConfig.Create(DefaultConfig.Instance)
                 .HideColumns("Type", "Method")
-                .AddColumn(new CustomColumn("Day", ExtractDayNumber))
-                .AddColumn(new CustomColumn("Part", ExtractDayPart))
+                .AddColumn(new CustomColumn("Day", "Day of AdventOfCode", ExtractDayNumber))
+                .AddColumn(new CustomColumn("Part", "Part of the day", ExtractDayPart))
                 .AddJob(Job.MediumRun)
                 .WithOrderer(new DefaultOrderer(SummaryOrderPolicy.Declared, MethodOrderPolicy.Alphabetical)));
     }
@@ -35,23 +35,28 @@ public static class DayRunner
         return BenchmarkRunner.Run(benchmarkTypes,
             ManualConfig.Create(DefaultConfig.Instance)
                 .HideColumns("Type", "Method")
-                .AddColumn(new CustomColumn("Day", ExtractDayNumber))
-                .AddColumn(new CustomColumn("Part", ExtractDayPart))
-                .AddJob(Job.MediumRun)
+                .AddColumn(new CustomColumn("Day", "Day of AdventOfCode", ExtractDayNumber))
+                .AddColumn(new CustomColumn("Part", "Part of the day", ExtractDayPart))
+                .AddJob(Job.ShortRun)
                 .WithOrderer(new DefaultOrderer(SummaryOrderPolicy.Declared, MethodOrderPolicy.Alphabetical))
                 .WithOptions(ConfigOptions.JoinSummary));
     }
 
     private static string ExtractDayNumber(BenchmarkCase benchmarkCase)
-        => benchmarkCase.Descriptor.Type.GenericTypeArguments.First().Name;
+        => benchmarkCase.Descriptor.Type.GenericTypeArguments.First().Name.Replace(nameof(Day_), "");
 
     private static string ExtractDayPart(BenchmarkCase benchmarkCase)
-        => benchmarkCase.Descriptor.WorkloadMethod.Name;
+        => benchmarkCase.Descriptor.WorkloadMethod.Name switch 
+            { 
+                nameof(DayBenchmarkWrapper<IDay>.PartOne) => "One", 
+                nameof(DayBenchmarkWrapper<IDay>.PartTwo) => "Two", 
+                _ => "???" 
+            };
 
     public static Type GetBenchmarkWrapperType(Type type)
     {
         var typeString = $"AdventOfCode.Infrastructure.DayBenchmarkWrapper`1[[{type.Name}, AdventOfCode, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null]]";
-        return Type.GetType(typeString);
+        return Type.GetType(typeString) ?? throw new InvalidOperationException($"Unable to resolve benchmark of type {typeString}");
     }
     public class CustomColumn : IColumn
     {
@@ -59,11 +64,12 @@ public static class DayRunner
         public string Id { get; }
         public string ColumnName { get; }
 
-        public CustomColumn(string columnName, Func<BenchmarkCase, string> textSelector)
+        public CustomColumn(string columnName, string legend, Func<BenchmarkCase, string> textSelector)
         {
             ColumnName = columnName;
             Id = nameof(CustomColumn) + "." + ColumnName;
             TextSelector =  textSelector;
+            Legend = legend;
         }
 
         public bool IsDefault(Summary summary, BenchmarkCase benchmarkCase) => false;
@@ -75,7 +81,7 @@ public static class DayRunner
         public int PriorityInCategory => 0;
         public bool IsNumeric => false;
         public UnitType UnitType => UnitType.Dimensionless;
-        public string Legend => $"Day of AdventOfCode";
+        public string Legend { get; }
         public string GetValue(Summary summary, BenchmarkCase benchmarkCase, SummaryStyle style) => GetValue(summary, benchmarkCase);
         public override string ToString() => ColumnName;
     }
